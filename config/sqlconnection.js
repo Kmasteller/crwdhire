6cc85b399959ee44e952b4005fb7b104dbd7454d
Kmasteller <kennethmasteller@gmail.com>
2018-01-15 19:07:53 -0700

    main handlebars basic layout - sql or heroku added - favicon - gitignore

--- config/sqlconnection.js
+++ config/sqlconnection.js
@@ -1,13 +1,18 @@
 // Setup SQL
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "crwdhire_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+      port: 3306,
+      host: "localhost",
+      user: "root",
+      password: "root",
+      database: "crwdhire_db"
+    });
+}
 
 // Make Connection
 connection.connect(function (err) {
