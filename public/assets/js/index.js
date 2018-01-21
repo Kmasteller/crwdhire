11c45e5bf74c89a196fc2fdffc35eec802a453b0
sallybananas <saljgiani@gmail.com>
2018-01-20 21:59:14 -0700

    idk

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -6,7 +6,32 @@ $('.nav-toggle').click(function(){
 	// });
 
 	$('.nav-main').toggleClass('off on');
-	
-
 
 });
+
+$(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
+
+// Modal Controls for loading on Document Ready
+// $(document).ready(function(){
+//     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+// $('#modal1').modal('open');
+// });
+
+// Modal Attrs
+$('.modal').modal({
+	dismissible: true, // Modal can be dismissed by clicking outside of the modal
+	opacity: .5, // Opacity of modal background
+	inDuration: 300, // Transition in duration
+	outDuration: 200, // Transition out duration
+	startingTop: '4%', // Starting top style attribute
+	endingTop: '10%', // Ending top style attribute
+	ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+	  alert("Ready");
+	  console.log(modal, trigger);
+	},
+	complete: function() { alert('Closed'); } // Callback for Modal close
+  }
+);
